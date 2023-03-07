febd2e47d34dbc71dc5968dad279c461af82bdd5
jackmpauly <pauly@chapman.edu>
2023-03-07 10:35:26 -0800

    Added basic java graphics

---
+++ Graph.java
@@ -0,0 +1,18 @@
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+public class Graph extends Canvas {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My Graph");
+        Canvas canvas = new Graph();
+        canvas.setSize(400, 400);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+        g.fillOval(100, 100, 200, 200);
+    }
+}
